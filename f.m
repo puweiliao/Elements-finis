9ffe19ed1e60ec474828263e282ed801bff7711e
puweiliao <liaopuweifr@gmail.com>
2021-12-11 18:12:20 +0100

    Create f.m

---
+++ f.m
@@ -0,0 +1,23 @@
+function val = f(x,y)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% f :
+% Evaluation de la fonction second membre.
+%
+% SYNOPSIS val = f(x,y)
+%          
+% INPUT * x,y : les 2 coordonnees du point ou on veut evaluer la fonction.
+%
+% OUTPUT - val: valeur de la fonction sur ce point.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+val=(2*pi^2)*sin(pi*x)*sin(pi*y);
+
+%val = 600*exp(-((x-1)/0.8).^2-((y-1)/0.8).^2);
+
+% A CHANGER POUR LA VALIDATION
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                     fin de la fonction
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%2020
